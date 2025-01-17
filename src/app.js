de8eee2c4edf9121516d54f9c0cdce3eb2f44682
dmmdeveloper <zongdostm@gmail.com>
2025-01-17 08:47:11 -0800

    increse themulter limit-2

--- src/app.js
+++ src/app.js
@@ -6,8 +6,8 @@ const app = express();
 
 //| App Configuration ||||
 
-app.use(express.json({limit:"200kb"}))
-app.use(express.urlencoded({limit:"200kb" , extended : true}))
+app.use(express.json({limit:"50mb"}))
+app.use(express.urlencoded({limit:"50mb" , extended : true}))
 app.use(express.static("public"))
 app.use(cookieParser());
 app.use(cors({
