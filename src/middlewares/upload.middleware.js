77b8391fa7c3a890d92c81a622398cd270475d68
dmmdeveloper <zongdostm@gmail.com>
2025-01-17 04:30:22 -0800

    file-2

--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -1,20 +1,17 @@
-import fs from "fs";
 import multer from "multer";
+import os from "node:os"
 
-// Ensure the directory exists
-const TEMP_DIR = "public/temp";
-if (!fs.existsSync(TEMP_DIR)) {
-    fs.mkdirSync(TEMP_DIR, { recursive: true });
-}
+// 
 
-// Multer Configuration
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, TEMP_DIR);
+// Configuration
+const storage  = multer.diskStorage({
+    destination : function (req , file , cb) {
+        const tmpDir  =os.tmpdir(); 
+        cb(null , tmpDir)
     },
-    filename: function (req, file, cb) {
-        cb(null, `${Math.floor(Math.random() * 9999)}_${file.originalname}`);
-    },
-});
+    filename : function (req , file , cb) {
+        cb(null  , `${Math.floor(Math.random()*9999)}_${file.originalname}`)        
+    }
+})
 
-export const upload = multer({ storage });
+export const upload = multer({storage})
