5cb5acd795a69fa7de03429b4ffc0ad2b45b4ecf
dmmdeveloper <zongdostm@gmail.com>
2025-01-27 03:06:09 -1000

    set .env

--- src/app.js
+++ src/app.js
@@ -17,22 +17,27 @@ app.use(cors({
     methods:["POST", "GET"]
 }));
 
-
 // Dafault Route "/"
-app.get("/" , (req ,res)=>{    
-    res.send(`<h1> Hello Dear Dost Muhammad , \n  You Are the dM-Developer  <hr> ${process.env?.HELLO} <hr> Email : ${process.env.COMPANYeMAIL} </h1>   `)
+app.get("/" , (req,res)=>{    
+    res.send(`<h3> Hello Dear Dost Muhammad , \n  You Are the dM-Developer  <hr> ${process.env?.HELLO} <hr> Email : ${process.env.COMPANYeMAIL} <br> PORT :   ${process.env.PORT} ,
+        <br> CLOUDnAME:   ${process.env.CLOUDnAME} 
+        <br> CLOUDaPIkEY :   ${process.env.CLOUDaPIkEY} 
+        // <br> CLOUDaPIsECRET :   ${process.env.CLOUDaPIsECRET} 
+        // <br> COMPANYeMAIL:   ${process.env.COMPANYeMAIL} 
+        // <br> EMAILpASSWORD :   ${process.env.EMAILpASSWORD} 
+        // <br> TOKENsECRET :   ${process.env.TOKENsECRET} 
+
+
+ </h3> 
+        `)
 })
 
 
 // Routes Importing
 
 import memberRouter from "./routes/member.routes.js";
-
 // Routes Declaration
-
 app.use("/member", memberRouter)
-
-
 
 export default app;
 
